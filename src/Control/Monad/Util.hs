2304601a6f5cf535469af8d8f6d2df22eb5cdcd6
YongJoon Joe <developer+some-utils@quietjoon.net>
2018-03-29 10:27:52 +0900

    Replace retryThisWhileUntil

--- src/Control/Monad/Util.hs
+++ src/Control/Monad/Util.hs
@@ -17,19 +17,41 @@ retryThisWhile f chk = do
     then retryThisWhile f chk
     else return v
 
-rtwu :: forall (m :: * -> *) b. Monad m => b -> Int -> m b -> (b -> Bool) -> m b
+rtwu :: forall (m :: * -> *) b. Monad m => Int -> m b -> (b -> Bool) -> m b
 rtwu = retryThisWhileUntil
+
+rtwud :: forall (m :: * -> *) b. Monad m => b -> Int -> m b -> (b -> Bool) -> m b
+rtwud = retryThisWhileUntilDefault
+
+rtwum :: forall (m :: * -> *) b. Monad m => Int -> m b -> (b -> Bool) -> m (Maybe b)
+rtwum = retryThisWhileUntilM
+
 {-# INLINE retryThisWhileUntil #-}
-retryThisWhileUntil :: forall (m :: * -> *) b. Monad m => b -> Int -> m b -> (b -> Bool) -> m b
-retryThisWhileUntil def num f chk =
-#ifdef LIMITEDDEBUG
-  assert (chk def) $
-#endif
+retryThisWhileUntil :: forall (m :: * -> *) b. Monad m => Int -> m b -> (b -> Bool) -> m b
+retryThisWhileUntil num f chk =
     retryThisWhileUntilSub num
   where
-    retryThisWhileUntilSub 0 = return def
+    retryThisWhileUntilSub 1 = f
     retryThisWhileUntilSub num = do
       v <- f
       if chk v
         then retryThisWhileUntilSub (num-1)
         else return v
+
+{-# INLINE retryThisWhileUntilDefault #-}
+retryThisWhileUntilDefault :: forall (m :: * -> *) b. Monad m => b -> Int -> m b -> (b -> Bool) -> m b
+retryThisWhileUntilDefault def num f chk = do
+  v <- retryThisWhileUntil num f chk
+  return $
+    if chk v
+      then v
+      else def
+
+{-# INLINE retryThisWhileUntilM #-}
+retryThisWhileUntilM :: forall (m :: * -> *) b. Monad m => Int -> m b -> (b -> Bool) -> m (Maybe b)
+retryThisWhileUntilM num f chk = do
+  v <- retryThisWhileUntil num f chk
+  return $
+    if chk v
+      then Just v
+      else Nothing
